836021282d490fa0840184cfa8d865e05192ac6f
Justin Strawther <jstrawther@resourcedata.com>
2021-09-23 20:40:32 -0500

    refactor IWebClient

--- WebWatcher.Core/WebClient/WebClient.cs
+++ WebWatcher.Core/WebClient/WebClient.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Net.Http;
+﻿using System.Net.Http;
 using System.Threading.Tasks;
-using WebWatcher.Core.Models;
 
 namespace WebWatcher.Core.WebClient
 {
@@ -14,28 +12,17 @@ namespace WebWatcher.Core.WebClient
             _httpClient = httpClientFactory.CreateClient();
         }
 
-        // TODO: return a processing result object with errors/warnings.
-        // Warn if xpath expression fails to match element.
-        public async Task<string> GetContentAsync(Website website)
+        public async Task<string> GetAsync(string url)
         {
             _httpClient.DefaultRequestHeaders.Add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36");
-            var urlToFetch = website.ContentUrl ?? website.DisplayUrl;
-            var response = await _httpClient.GetAsync(urlToFetch);
+
+            var response = await _httpClient.GetAsync(url);
+
             response.EnsureSuccessStatusCode();
 
             var content = await response.Content.ReadAsStringAsync();
 
-            // filter by optional element selector xpath
-            if (!string.IsNullOrEmpty(website.ElementSelector))
-            {
-                var filteredContent = HtmlParser.SelectNode(content, website.ElementSelector);
-                if (!string.IsNullOrEmpty(filteredContent))
-                {
-                    content = filteredContent;
-                }
-            }
-
-            return content;            
+            return content;
         }
     }
 }
